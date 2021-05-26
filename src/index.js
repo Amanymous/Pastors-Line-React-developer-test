0527c00d4107571ab80d2ea93c5a5b063893ca27
amanymous <aman.mirza9@yahoo.com>
2021-05-26 23:55:31 +0500

    feat: new code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-// import { createLogger } from 'redux-logger'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import App from './App';
-import reducer from './redux/reducers/reducer'
-import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App';
+import store from './redux/store';
+import reportWebVitals from './reportWebVitals';
 
-// import reportWebVitals from './reportWebVitals';
-
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk))
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
